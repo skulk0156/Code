89283d28441ca44d26a54bb24017ed22ef7a1681
skulk0156 <skulk156.pc@gmail.com>
2025-11-05 18:25:43 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '../src/components/auth/Login';
 import Dashboard from './components/Dashboard';
+// import ProfilePage from './pages/ProfilePage'; // Import ProfilePage
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/profile" element={<ProfilePage />} /> New route */}
       </Routes>
     </Router>
   );
